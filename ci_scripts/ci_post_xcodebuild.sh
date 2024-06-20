1386ef9f5c656d28b128853ddc522c4a6ae04adf
Adrian <adri4silva@gmail.com>
2024-06-20 20:57:00 +0200

    Add echo

---
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#  ci_post_xcodebuild.sh
+#  FoodTruck
+#
+#  Created by Adrián Silva on 20/6/24.
+#  
--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -5,5 +5,6 @@
 
 echo 'Change Beta App Icon'
 APP_ICON_PATH="$CI_PRIMARY_REPOSITORY_PATH/FoodTruck/Assets.xcassets/AppIcon.appiconset"
+echo APP_ICON_PATH
 rm -rf $APP_ICON_PATH
 mv "$CI_PRIMARY_REPOSITORY_PATH/ci_scripts/AppIcon-Beta.appiconset" $APP_ICON_PATH
