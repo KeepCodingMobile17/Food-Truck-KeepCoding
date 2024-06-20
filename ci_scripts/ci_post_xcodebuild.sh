90c69217081278b4e4e2026a75c2392278be841d
Adrian <adri4silva@gmail.com>
2024-06-20 21:14:23 +0200

    Remove scripts

--- ci_scripts/ci_post_xcodebuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#  ci_post_xcodebuild.sh
-#  FoodTruck
-
-#TESTFLIGHT_DIR_PATH=../TestFlight
-#mkdir $TESTFLIGHT_DIR_PATH
-#git fetch --deepen 3 && git log -3 --pretty=format:"%s" >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
